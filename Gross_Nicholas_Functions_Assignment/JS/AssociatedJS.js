560b8890e4579639aedc10ab6ac6f2b4cbb867cd
NicholasGross <ngross@fullsail.edu>
2015-06-25 17:20:19 -0400

    2cd Commit Week 4

--- Gross_Nicholas_Functions_Assignment/JS/AssociatedJS.js
+++ Gross_Nicholas_Functions_Assignment/JS/AssociatedJS.js
@@ -22,3 +22,35 @@ var name=prompt("What is your name?");
 
 while(name==="")
 
+
+    //reprompting the user
+
+    if (name ==="") {
+        name = prompt("Please do not leave blank.\nEnter your name.");
+    } else if ((name)) {
+        name = prompt("Please do not leave blank.\nEnter your name. ");
+    }
+
+console.log(name);
+
+//Now we will see how much gas can be held in your tank
+
+var fullTank=prompt("Enter the amount of gas that your tank can hold in gallons?");
+
+
+
+//looking to see if prompt was left empty
+
+while(fullTank==="")
+
+
+    //reprompting the user
+
+    if (fullTank ==="") {
+        fullTank = prompt("Do not leave blank.\nHow much gas can your gas tank hold?");
+    } else if (fullTank) {
+        fullTank = prompt("Do not leave blank.\nHow much gas can your gas tank hold?");
+    }
+
+console.log(fullTank);
+.
