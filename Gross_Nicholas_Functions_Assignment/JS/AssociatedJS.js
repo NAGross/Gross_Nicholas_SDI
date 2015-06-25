78782d3b74c592ddda213022ec7b57732df4ae00
NicholasGross <ngross@fullsail.edu>
2015-06-25 17:19:21 -0400

    1st commit Week 4

---
+++ Gross_Nicholas_Functions_Assignment/JS/AssociatedJS.js
@@ -0,0 +1,24 @@
+/*
+ Nicholas Gross
+ Week 4
+ 06/24/2015
+ Functions Assignment
+ */
+
+
+
+//Finding out how much gas is left in your has tank
+
+
+// Welcome the user
+
+alert("Welcome, Today we will find out how much gas is \nleft in your gas tank!");
+
+//Find out the users name
+
+var name=prompt("What is your name?");
+
+//looking to see if the variable is empty
+
+while(name==="")
+
